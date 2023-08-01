8f34bc61bfb7a8cc7479fcdbb71db2d56a6bd05a
Yan Tavares <yantavares2002@gmail.com>
2023-08-01 11:38:18 -0300

    img for modes

--- script.js
+++ script.js
@@ -7,5 +7,12 @@ function toggleMode() {
         html.classList.add('light')
     }
 
+    const img = document.querySelector("#profile img")
+    if(html.classList.contains('light')){
+        img.setAttribute('src','./assets/avatar_light.jfif')
+    } else {
+        img.setAttribute('src','./assets/avatar_dark.jfif')
+    }
+
 
 }
